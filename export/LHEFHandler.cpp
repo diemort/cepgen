2e6546fffd548a793ad258f930064bc5c550ed79
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-06-14 12:27:53 +0200

    Several improvements in many parts of the code * slightly more robust integration function definition * moved all objects members names to camelBack/mixedCase notation * moved private attributes to trailing underscore notation

---
+++ export/LHEFHandler.cpp
@@ -0,0 +1,40 @@
+#include "LHEFHandler.h"
+
+#if HEPMC_VERSION_CODE>=3000000
+
+namespace OutputHandler
+{
+  LHEFHandler::LHEFHandler( const char* filename ) :
+    ExportHandler( ExportHandler::LHE )
+  {
+    output = new LHEF::Writer( filename );
+  }
+
+  void
+  LHEFHandler::operator<<( const Event* ev )
+  {
+    fillEvent( ev );
+    output->writeEvent();
+    clearEvent();
+  }
+
+  void
+  LHEFHandler::fillEvent( const Event* ev )
+  {
+    // ... do whatever is needed for output->hepeup
+    ConstParticlesRef part_vec = ev->constParticlesRef();
+    //HEPEUT*
+    for ( unsigned int i=0; i<part_vec.size(); i++ ) {
+
+    }
+  }
+
+  void
+  LHEFHandler::clearEvent()
+  {
+    // ...
+    output->hepeup.clear();
+  }
+}
+
+#endif
